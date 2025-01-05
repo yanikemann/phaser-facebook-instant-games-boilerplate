77be01bc1810478092df409fdb3d9a873b2dbeb0
yanikemann <yanikemann@yeoworks.io>
2025-01-05 07:08:31 +0000

    updated build config

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,22 @@
 import { defineConfig } from 'vite';
+import copy from 'rollup-plugin-copy';
 
 export default defineConfig({
-  base: './',
   build: {
-    outDir: 'dist',
-    emptyOutDir: true,
     rollupOptions: {
-        input: {
-          main: 'index.html',
-          'fbapp-config.json': 'fbapp-config.json',
-        },
-        output: {
-          entryFileNames: '[name]', // Output file names
-        },
-      },
+      input: 'index.html', // Entry point for the app
+    },
+    assetsInlineLimit: 0, // Ensures assets aren't inlined as base64
+    outDir: 'dist', // Output directory for the build
+    emptyOutDir: true, // Clears the output directory before building
   },
+  plugins: [
+    copy({
+      targets: [
+        { src: 'assets/**/*', dest: 'dist/assets' }, // Copy all files from 'assets/' to 'dist/assets'
+        { src: 'fbapp-config.json', dest: 'dist' }, // Copy 'fbapp-config.json' to 'dist'
+      ],
+      hook: 'writeBundle', // Ensure files are copied after the build is done
+    }),
+  ],
 });
