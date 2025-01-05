2e6d1f94a7a295ec98a387bf86026dd0e870ed2a
yanikemann <yanikemann@yeoworks.io>
2025-01-05 01:08:57 +0000

    working files

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,18 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-    base: './',
+  base: './',
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true,
+    rollupOptions: {
+        input: {
+          main: 'index.html',
+          'fbapp-config.json': 'fbapp-config.json',
+        },
+        output: {
+          entryFileNames: '[name]', // Output file names
+        },
+      },
+  },
 });
